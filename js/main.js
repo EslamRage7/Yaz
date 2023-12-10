aefc4a159e99c19a2057b4db1a38f884958ac383
Eslam Rageh <111660362+EslamRage7@users.noreply.github.com>
2023-12-11 00:32:20 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let allLis = document.querySelectorAll("ul li");
+let homeLi = document.querySelector(".home-icon");
+let aboutLi = document.querySelector(".about-icon");
+let clintLi = document.querySelector(".clint-icon");
+let contactLi = document.querySelector(".contact-icon");
+
+window.onscroll = function () {
+  if (this.scrollY < 600) {
+    allLis.forEach((ele) => {
+      ele.classList.remove("active");
+      homeLi.classList.add("active");
+    });
+  } else if (this.scrollY > 600 && this.scrollY < 1250) {
+    allLis.forEach((ele) => {
+      ele.classList.remove("active");
+      aboutLi.classList.add("active");
+    });
+  } else if (this.scrollY > 1250 && this.scrollY < 1800) {
+    allLis.forEach((ele) => {
+      ele.classList.remove("active");
+      clintLi.classList.add("active");
+    });
+  } else if (this.scrollY > 1800) {
+    allLis.forEach((ele) => {
+      ele.classList.remove("active");
+      contactLi.classList.add("active");
+    });
+  }
+};
