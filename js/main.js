ad2ef876d4c63c7f7f2f644de85f68498f972194
Eslam Rageh <111660362+EslamRage7@users.noreply.github.com>
2023-12-14 22:07:39 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,22 +5,22 @@ let clintLi = document.querySelector(".clint-icon");
 let contactLi = document.querySelector(".contact-icon");
 
 window.onscroll = function () {
-  if (this.scrollY < 600) {
+  if (this.scrollY < 450) {
     allLis.forEach((ele) => {
       ele.classList.remove("active");
       homeLi.classList.add("active");
     });
-  } else if (this.scrollY > 600 && this.scrollY < 1250) {
+  } else if (this.scrollY > 450 && this.scrollY < 1180) {
     allLis.forEach((ele) => {
       ele.classList.remove("active");
       aboutLi.classList.add("active");
     });
-  } else if (this.scrollY > 1250 && this.scrollY < 1800) {
+  } else if (this.scrollY > 1180 && this.scrollY < 1650) {
     allLis.forEach((ele) => {
       ele.classList.remove("active");
       clintLi.classList.add("active");
     });
-  } else if (this.scrollY > 1800) {
+  } else if (this.scrollY > 1650) {
     allLis.forEach((ele) => {
       ele.classList.remove("active");
       contactLi.classList.add("active");
